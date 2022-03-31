c3b5a7c89852945ef9dac8e1ea9fad4434f6b667
kenliii <90875261+kenliii@users.noreply.github.com>
2022-03-31 15:00:45 -0700

    made edit button

--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,7 @@ from asyncio import Task
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 from .models import Task
 
@@ -16,6 +16,11 @@ class TaskDetail(DetailView):
     template_name = 'main_app/task.html'
 
 class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
