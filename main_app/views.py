cd3270c70396837713399bf1cf47f3bcb8c24ba0
kenliii <90875261+kenliii@users.noreply.github.com>
2022-03-30 23:14:11 -0700

    create item is done

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,8 @@ from asyncio import Task
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import Task
 
 class TaskList(ListView):
@@ -12,3 +14,8 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'main_app/task.html'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
