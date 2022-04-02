6a0d4b76375c8aeb0cb25107b22bab874e7c6f6b
kenliii <90875261+kenliii@users.noreply.github.com>
2022-04-02 10:46:10 -0700

    made a login and logout

--- main_app/views.py
+++ main_app/views.py
@@ -4,7 +4,16 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
 from .models import Task
+
+class CustomLoginView(LoginView):
+    template_name= 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_successurl(self):
+        return reverse_lazy('tasks')
 
 class TaskList(ListView):
     model = Task
